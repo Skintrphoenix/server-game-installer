b968b18a01669e8a10613216ca90de7ccdf58ede
Skintrphoenix <72775104+Skintrphoenix@users.noreply.github.com>
2021-05-19 13:17:14 +0700

    Update php.sh

--- mcpe/bin/php.sh
+++ mcpe/bin/php.sh
@@ -4,3 +4,4 @@ wget -q https://jenkins.pmmp.io/job/PHP-7.4-Aggregate/lastSuccessfulBuild/artifa
 tar -xf PHP-7.4-Linux-x86_64.tar.gz 
 EXTENSION_DIR=$(find "$(pwd)/bin" -name *debug-zts*) 
 grep -q '^extension_dir' bin/php7/bin/php.ini && sed -i'bak' "s{^extension_dir=.*{extension_dir=\"$EXTENSION_DIR\"{" bin/php7/bin/php.ini || echo "extension_dir=\"$EXTENSION_DIR\"" >> bin/php7/bin/php.ini
+rm -rf PHP-7.4-Linux-x86_64.tar.gz

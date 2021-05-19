56aaf69ef3fcc68b67ce092ffd91819d782d1380
Skintrphoenix <72775104+Skintrphoenix@users.noreply.github.com>
2021-05-19 08:45:19 +0700

    Update yatopia.sh

--- mcje/yatopia.sh
+++ mcje/yatopia.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-if wget -q https://ci.codemc.io/job/YatopiaMC/job/Yatopia/job/ver%252F1.16.5/lastSuccessfulBuild/artifact/target/yatopia-1.16.5-paperclip-b115.jar;
+version=1.16.5
+build=115
+
+if wget -q https://ci.codemc.io/job/YatopiaMC/job/Yatopia/job/ver%252F$version/lastSuccessfulBuild/artifact/target/yatopia-$version-paperclip-b$build.jar;
 then
  echo "Install Yatopia"
 fi
+
+mv yatopia-$version-paperclip-b$build.jar yatopia.jar

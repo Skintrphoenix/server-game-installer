2af0b52fb083c2b5c6f98bcd2b43deb1efde77de
Skintrphoenix <72775104+Skintrphoenix@users.noreply.github.com>
2021-05-19 08:41:28 +0700

    Update altay.sh

--- mcpe/altay.sh
+++ mcpe/altay.sh
@@ -4,3 +4,5 @@ if wget -q https://altay.minehub.de/job/Altay/lastSuccessfulBuild/artifact/Altay
 then
 echo "install Altay"
 fi
+
+wget -q https://raw.githubusercontent.com/Skintrphoenix/server-game-installer/main/mcpe/data/pocketmine.yml

6c62cb8dfa92a113a1407ff5179f5e1fdaa1624e
Skintrphoenix <72775104+Skintrphoenix@users.noreply.github.com>
2021-05-19 08:38:37 +0700

    Create yatopia.sh

---
+++ mcje/yatopia.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+if wget -q https://ci.codemc.io/job/YatopiaMC/job/Yatopia/job/ver%252F1.16.5/lastSuccessfulBuild/artifact/target/yatopia-1.16.5-paperclip-b115.jar;
+then
+ echo "Install Yatopia"
+fi
